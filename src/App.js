941509402ac3161c4c3709917e0405a1af0126e5
Inderpreet <inderpreet.react.project@gmail.com>
2022-10-01 02:26:27 +0530

    404 page not found added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Chat from "./pages/Chat";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Route path="/" element={<Login />} />
 			<Route path="/signup" element={<Signup />} />
 			<Route path="/chat" element={<Chat />} />
+			<Route path="*" element={<PageNotFound />} />
 		</Routes>
 	);
 }
