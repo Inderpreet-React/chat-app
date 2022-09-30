8c8d89a9440ed8f3163f6086a9b41ae49f075a67
Inderpreet <inderpreet.react.project@gmail.com>
2022-10-01 00:18:41 +0530

    Routes created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			<Login />
-		</div>
+		<Routes>
+			<Route path="/" element={<Login />} />
+		</Routes>
 	);
 }
 
