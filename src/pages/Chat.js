3bae1b0d0a3cb1bbf696b59748a1aab8aa7a640d
Inderpreet <inderpreet.react.project@gmail.com>
2022-10-01 02:14:24 +0530

    chat page initial design

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,11 +1,29 @@
 import React from "react";
+import PageWrapper from "../PageWrapper";
+import MessagingSvg from "../images/messagingSvg.svg";
+import Avatar from "../images/avatar.png";
 
 export default function Chat() {
 	return (
-		<div className="h-screen w-full bg-gray-100 md:p-8">
-			<div className="relative flex h-full w-full flex-col items-center justify-center gap-6 border-2 border-gray-800 bg-indigo-100 pt-0 md:flex-row md:justify-start md:p-8">
-				Chat
+		<PageWrapper additionalClasses="divide-y-2 md:divide-y-0 md:divide-x-2 divide-indigo-500">
+			<div className="flex h-1/3 w-full flex-col gap-4 p-2 md:h-full md:w-1/3 md:p-0">
+				<div className="flex justify-between">
+					<h1 className="flex h-full items-end text-2xl font-semibold text-indigo-500">
+						Bruh Chat
+					</h1>
+					<img src={Avatar} alt="Avatar" className="h-10 w-10" />
+				</div>
+				<input type="text" placeholder="Search" className="w-full" />
 			</div>
-		</div>
+			<div className="h-2/3 w-full p-2 md:h-full md:w-2/3 md:p-0 md:pl-8">
+				<div className="flex h-full w-full items-center justify-center">
+					<img
+						src={MessagingSvg}
+						alt="Messaging"
+						className="h-72 w-72 md:h-96 md:w-96"
+					/>
+				</div>
+			</div>
+		</PageWrapper>
 	);
 }
